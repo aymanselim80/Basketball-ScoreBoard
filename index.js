ceac52e4a0f3f3d610e0307b63352cca2e2a413a
Ayman Selim <aymansleam80@gmail.com>
2022-09-04 14:24:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let home_el = document.getElementById("home")
+let guest_el = document.getElementById("guest")
+
+let home = 0 
+let guest = 0
+
+function homePlusOne()
+{
+    home += 1
+    home_el.textContent = home
+}
+function homePlusTwo()
+{
+    home += 2
+    home_el.textContent = home
+}
+function homePlusThree()
+{
+    home += 3
+    home_el.textContent = home
+}
+function guestPlusOne()
+{
+    guest += 1
+    guest_el.textContent = guest
+}
+function guestPlusTwo()
+{
+    guest += 2
+    guest_el.textContent = guest
+}function guestPlusThree()
+{
+    guest += 3
+    guest_el.textContent = guest
+}
